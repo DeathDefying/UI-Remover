97f0b5861d4f272676e271a874d51a25ec3a371d
Foglio1024 <foglio1024@gmail.com>
2020-09-23 21:39:44 +0200

    add bat to generate manifests

---
+++ generate.bat
@@ -0,0 +1,6 @@
+node D:\Repos\tcc-stub\manifest-generator.js D:\Repos\toolbox-client-mods\UI-Remover\remove-BossWindow
+node D:\Repos\tcc-stub\manifest-generator.js D:\Repos\toolbox-client-mods\UI-Remover\remove-BuffsWindow
+node D:\Repos\tcc-stub\manifest-generator.js D:\Repos\toolbox-client-mods\UI-Remover\remove-CharacterWindow
+node D:\Repos\tcc-stub\manifest-generator.js D:\Repos\toolbox-client-mods\UI-Remover\remove-FlightGauge
+node D:\Repos\tcc-stub\manifest-generator.js D:\Repos\toolbox-client-mods\UI-Remover\remove-LfgWindow
+node D:\Repos\tcc-stub\manifest-generator.js D:\Repos\toolbox-client-mods\UI-Remover\remove-PartyWindow
