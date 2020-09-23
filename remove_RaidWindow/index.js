1a02c9172cbde5d5d91ef53c80f01b6ee090222b
Foglio1024 <foglio1024@gmail.com>
2020-09-23 21:40:24 +0200

    delete unnecessary files

--- remove_RaidWindow/index.js
+++
@@ -1,9 +0,0 @@
-const config = require('./config.json');
-module.exports = function RaidWindow(m) {
-    let disabled = config.disabled;
-    if(!disabled)
-	{
-        m.installGPK("S1UI_PartyWindowRaidInfo.gpk");
-	}
-    return;
-}
--- remove_TeraRewards/index.js
+++
@@ -1,9 +0,0 @@
-const config = require('./config.json');
-module.exports = function TeraRewards(m) {
-    let disabled = config.disabled;
-    if(!disabled)
-	{
-        m.installGPK("S1UI_Benefit.gpk");
-	}
-    return;
-}
