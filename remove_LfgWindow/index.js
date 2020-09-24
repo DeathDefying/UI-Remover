74f49ead0932d8d9cfe15febead5f312b60e2288
Foglio1024 <foglio1024@gmail.com>
2020-09-24 13:03:26 +0200

    oops

--- remove_LfgWindow/index.js
+++ remove_LfgWindow/index.js
@@ -2,11 +2,10 @@ exports.ClientMod = class
 {
     constructor(mod)
     {
+        mod.log("This mod has been merged into TCC main module and can be uninstalled.");
+        return;
         this.install = function(installer)
         {
-            mod.log("This mod has been merged into TCC main module and can be uninstalled.");
-            return;
-    
             installer.gpk("S1UI_PartyBoard.gpk");
             installer.gpk("S1UI_PartyBoardMemberInfo.gpk");
         };
