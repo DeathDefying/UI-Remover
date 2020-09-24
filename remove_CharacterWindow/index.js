766f58e0b93cd569d689563fe554092096c70858
Foglio1024 <foglio1024@gmail.com>
2020-09-24 13:02:13 +0200

    disable mods as they're merged into TCC main module

--- remove_CharacterWindow/index.js
+++ remove_CharacterWindow/index.js
@@ -2,6 +2,9 @@ exports.ClientMod = class
 {
     constructor(mod)
     {
+        mod.log("This mod has been merged into TCC main module and can be uninstalled.");
+        return;
+
         this.install = function(installer)
         {
             installer.gpk("S1UI_CharacterWindow.gpk");
--- remove_LfgWindow/index.js
+++ remove_LfgWindow/index.js
@@ -4,6 +4,9 @@ exports.ClientMod = class
     {
         this.install = function(installer)
         {
+            mod.log("This mod has been merged into TCC main module and can be uninstalled.");
+            return;
+    
             installer.gpk("S1UI_PartyBoard.gpk");
             installer.gpk("S1UI_PartyBoardMemberInfo.gpk");
         };
